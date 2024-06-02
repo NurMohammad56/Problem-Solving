557a4bd66e80cfdaf499669cae4ee95f51f467c9
Nur Mohammad <milonsardar56@gmail.com>
2024-06-02 23:57:40 +0600

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,3 +1,22 @@
+/*
+1. You are given a string s. The score of a string
+   is defined as the sum of the absolute difference between
+   the ASCII values of adjacent characters.
+    Return the score of s.
+    Example 1:
+    Input: s = "hello"
+    
+    Output: 13
+
+Explanation:
+
+    The ASCII values of the characters
+    in s are: 'h' = 104, 'e' = 101, 'l' = 108,
+    'o' = 111. So, the score of s would be
+    |104 - 101| + |101 - 108| + |108 - 108| + 
+    |108 - 111| = 3 + 7 + 0 + 3 = 13.
+*/
+
 const scoreOfString = function (s) {
   let score = 0;
   for (let i = 0; i < s.length - 1; i++) {
