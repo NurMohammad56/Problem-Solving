3c524b081c263605df65c22b1e9ab63db8f73631
Nur Mohammad <milonsardar56@gmail.com>
2024-08-23 23:39:56 +0600

    Generate Fibonacci Sequence

---
+++ Generate Fibonacci Sequence/index.js
@@ -0,0 +1,15 @@
+function* fibGenerator() {
+  let a = 0,
+    b = 1;
+  while (true) {
+    yield a;
+    [a, b] = [b, a + b];
+  }
+}
+
+const gen = fibGenerator();
+console.log(gen.next().value); // 0
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 2
+console.log(gen.next().value); // 3
