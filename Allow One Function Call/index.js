94dac62354e6f6249c1f0f18bd9d3daa38c3e5a5
Nur Mohammad <milonsardar56@gmail.com>
2024-09-16 01:34:57 +0600

    Allow one function call

---
+++ Allow One Function Call/index.js
@@ -0,0 +1,13 @@
+function once(fn) {
+  let called = false;
+  let result;
+
+  return function (...args) {
+    if (!called) {
+      called = true;
+      result = fn(...args);
+      return result;
+    }
+    return undefined;
+  };
+}
