a6d8524a2e90663bbb2cffd727ee664459d1af9d
Nur Mohammad <milonsardar56@gmail.com>
2024-09-23 17:48:09 +0600

    Longest Common Prefix

---
+++ Longest Common Prefix/index.js
@@ -0,0 +1,17 @@
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, -1);
+      if (prefix === "") return "";
+    }
+  }
+
+  return prefix;
+}
+
+const strs = ["flower", "flow", "flight"];
+console.log(longestCommonPrefix(strs));
