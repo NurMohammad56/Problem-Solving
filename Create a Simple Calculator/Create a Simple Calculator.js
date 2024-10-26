4394f5c1527a08f43913c1ea9ffa1dafb2f47c39
Nur Mohammad <milonsardar56@gmail.com>
2024-10-26 23:03:30 +0600

    Create a simple calculator

---
+++ Create a Simple Calculator/Create a Simple Calculator.js
@@ -0,0 +1,14 @@
+function calculator(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
+    default:
+      return "Invalid operator";
+  }
+}
