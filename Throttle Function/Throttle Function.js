ebe1ca64557eb336075f460ff74d1e4ef838e70a
Nur Mohammad <milonsardar56@gmail.com>
2024-11-05 23:21:57 +0600

    Throttle Function

---
+++ Throttle Function/Throttle Function.js
@@ -0,0 +1,23 @@
+function throttle(func, limit) {
+  let lastFunc;
+  let lastRan;
+  return function (...args) {
+    if (!lastRan) {
+      func.apply(this, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(() => {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(this, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+}
+
+// Usage
+const log = () => console.log("Throttled!");
+const throttledLog = throttle(log, 2000);
+throttledLog(); // Will log "Throttled!" at most once every 2 seconds
