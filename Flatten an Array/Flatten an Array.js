397aba70ec7b18a56a2c1d023463e115c2eee3b4
Nur Mohammad <milonsardar56@gmail.com>
2024-11-08 23:23:53 +0600

    Flatten an Array

---
+++ Flatten an Array/Flatten an Array.js
@@ -0,0 +1,10 @@
+v;
+function flattenArray(arr) {
+  return arr.reduce(
+    (acc, val) =>
+      Array.isArray(val) ? acc.concat(flattenArray(val)) : acc.concat(val),
+    []
+  );
+}
+
+console.log(flattenArray([1, [2, [3, 4], 5], 6]));
