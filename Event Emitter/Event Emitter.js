2ee7f4801b59594d2db3c2c0c4443b24bd61be0f
Nur Mohammad <milonsardar56@gmail.com>
2024-11-11 22:26:20 +0600

    Event Emitter

---
+++ Event Emitter/Event Emitter.js
@@ -0,0 +1,28 @@
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(event, listener) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(listener);
+  }
+
+  emit(event, ...args) {
+    if (this.events[event]) {
+      this.events[event].forEach((listener) => listener.apply(this, args));
+    }
+  }
+
+  off(event, listener) {
+    if (this.events[event]) {
+      this.events[event] = this.events[event].filter((l) => l !== listener);
+    }
+  }
+}
+
+const emitter = new EventEmitter();
+emitter.on("greet", (name) => console.log(`Hello, ${name}`));
+emitter.emit("greet", "World");
