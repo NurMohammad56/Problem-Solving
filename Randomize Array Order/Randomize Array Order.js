e300d41fd065de27e41a6a76c9cbbaa83132fabc
Nur Mohammad <milonsardar56@gmail.com>
2024-11-20 23:46:39 +0600

    Randomize Array Order

---
+++ Randomize Array Order/Randomize Array Order.js
@@ -0,0 +1,9 @@
+function shuffleArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+console.log(shuffleArray([1, 2, 3, 4, 5]));
