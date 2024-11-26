af094d03d63563841a5db8bebc76caa925a21527
Nur Mohammad <milonsardar56@gmail.com>
2024-11-26 23:53:52 +0600

    Implement a Queue Using Two Stacks

---
+++ Implement a Queue Using Two Stacks/Implement a Queue Using Two Stacks.js
@@ -0,0 +1,24 @@
+class Queue {
+  constructor() {
+    this.stack1 = [];
+    this.stack2 = [];
+  }
+
+  enqueue(item) {
+    this.stack1.push(item);
+  }
+
+  dequeue() {
+    if (this.stack2.length === 0) {
+      while (this.stack1.length > 0) {
+        this.stack2.push(this.stack1.pop());
+      }
+    }
+    return this.stack2.pop();
+  }
+}
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.dequeue();
